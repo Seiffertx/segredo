1c1ab812b4e2b0ac38ff0d66c9a5dd2881d23d10
Seiffertx <66689576+Seiffertx@users.noreply.github.com>
2020-12-01 10:05:40 -0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -4,12 +4,13 @@
             }
 
             function count() {
+                localStorage.setItem('counter', counter);
                 counter = counter + 1;
                 document.querySelector('h1').innerHTML = counter;
+                localStorage.setItem('counter', counter);
             }
 
             document.addEventListener('DOMContentLoaded', function() {
+                document.querySelector('h1').innerHTML = localStorage.getItem('counter')       
                 document.querySelector('button').onclick = count;
-
-                setInterval(count, 1000);
             });                
