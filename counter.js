5ac4f0270c68085fc4c29a796ef799a926e3e6d0
Seiffertx <66689576+Seiffertx@users.noreply.github.com>
2020-12-01 10:19:05 -0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,16 +1,15 @@
+if (!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
+}
 
-            if (!localStorage.getItem('counter')) {
-                localStorage.setItem('counter', 0);
-            }
+function count() {
+    let counter = localStorage.getItem('counter');
+    counter++;
+    document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
+}
 
-            function count() {
-                localStorage.setItem('counter', counter);
-                counter = counter + 1;
-                document.querySelector('h1').innerHTML = counter;
-                localStorage.setItem('counter', counter);
-            }
-
-            document.addEventListener('DOMContentLoaded', function() {
-                document.querySelector('h1').innerHTML = localStorage.getItem('counter')       
-                document.querySelector('button').onclick = count;
-            });                
+    document.addEventListener('DOMContentLoaded', function() {
+        document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+        document.querySelector('button').onclick = count;
+});
